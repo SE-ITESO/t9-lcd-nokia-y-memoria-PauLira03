1a3535a357212b1c12adb0a66b1a15931e65d5fe
PauLira03 <145362083+PauLira03@users.noreply.github.com>
2024-04-14 21:10:19 -0600

    primera version, inicializacion de spi de la memoria

---
+++ source/LCD_nokia_images.h
@@ -0,0 +1,27 @@
+/*
+ * LCD_nokia_images.h
+ *
+ *  Created on: 25/09/2018
+ *      Author: jlpe
+ */
+
+#ifndef LCD_NOKIA_IMAGES_H_
+#define LCD_NOKIA_IMAGES_H_
+
+#include "stdint.h"
+#define DATA_WIDTH			(2U)
+#define SRC_OFFSET			(2U)
+#define DEST_OFFSET			(0U)
+#define MINOR_LOOP_BYTES	(2U)
+#define MAJOR_LOOP_COUNTS	(20)
+
+
+#define LCD_img_base_addr_1 0x40000
+#define LCD_img_base_addr_2 0x41000
+#define LCD_img_base_addr_3 0x42000
+#define LCD_img_base_addr_4 0x43000
+
+#define LCD_img_base_addr_5 0x44000
+
+
+#endif /* LCD_NOKIA_IMAGES_H_ */
---
+++ source/main.c
@@ -0,0 +1,75 @@
+/**
+	\file 
+	\brief 
+		This is a starter file to use the Nokia 5510 LCD. 
+		The LCD is connected as follows:
+		RST-PDT5
+		CE-PTD0
+		DC-PTD4
+		DIN-PTD2
+		CLK-PTD1
+	\author J. Luis Pizano Escalante, luispizano@iteso.mx
+	\date	19/09/2019
+	\todo
+	    The SPI device driver needs to be completed.
+ */
+
+ 
+
+#include "LCD_nokia.h"
+#include "LCD_nokia_images.h"
+#include "stdint.h"
+#include "SPI.h"
+#include "Delay.h"
+
+
+/*! This array hold the initial picture that is shown in the LCD. Note that extern should be avoided*/
+extern const uint8_t ITESO[504];
+extern uint8_t img_1[504];
+extern uint8_t img_2[504];
+extern uint8_t img_3[504];
+extern uint8_t img_4[504];
+extern uint8_t img_5[504];
+
+int main(void)
+{
+
+	SPI_config_LCD();
+	SPI_config_MEM();
+
+	LCD_nokia_init(); /*! Configuration function for the LCD */
+
+	for(;;) {
+
+		LCD_nokia_clear();/*! It clears the information printed in the LCD*/
+
+		LCD_nokia_bitmap(ITESO); /*! It prints an array that hold an image, in this case is the initial picture*/
+		delay(65000);
+		LCD_nokia_clear();
+
+		LCD_nokia_bitmap(img_1);
+		delay(65000);
+		LCD_nokia_clear();
+
+		LCD_nokia_bitmap(img_2);
+		delay(65000);
+		LCD_nokia_clear();
+
+		LCD_nokia_bitmap(img_3);
+		delay(65000);
+		LCD_nokia_clear();
+
+		LCD_nokia_bitmap(img_4);
+		delay(65000);
+		LCD_nokia_clear();
+
+		LCD_nokia_bitmap(img_5);
+		delay(65000);
+		LCD_nokia_clear();
+
+		delay(65000);
+	}
+	
+	return 0;
+}
+
